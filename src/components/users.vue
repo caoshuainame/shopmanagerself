05487fb4595bff23e03b76e061847c7b1940c1fc
caoshuai <420126827@qq.com>
2019-03-25 14:37:18 +0800

    添加用户对话框

--- src/components/users.vue
+++ src/components/users.vue
@@ -9,10 +9,10 @@
         <!-- 搜索框 -->
         <el-row class="searchBox">
             <el-col>
-                <el-input class="searchInput" placeholder="请输入内容" v-model="query" >
-                    <el-button slot="append" icon="el-icon-search"></el-button>
+                <el-input @clear="getAllUsers()" clearable class="searchInput" placeholder="请输入内容" v-model="query" >
+                    <el-button @click="searchUser()" slot="append" icon="el-icon-search"></el-button>
                 </el-input>
-                <el-button type="primary">添加用户</el-button>
+                <el-button @click="showDiaAddUsers()" type="primary">添加用户</el-button>
             </el-col>
         </el-row>
         <!-- 表格区域 -->
@@ -56,6 +56,37 @@
               </template>
           </el-table-column>
         </el-table>
+        <!-- 分页 -->
+        <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="pagenum"
+          :page-sizes="[2, 4, 6, 8]"
+          :page-size="2"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="total">
+        </el-pagination>
+        <!-- 添加用户对话框 -->
+        <el-dialog title="收货地址" :visible.sync="dialogFormVisibleAdd">
+          <el-form label-position="left" label-width="80px" :model="formdata">
+            <el-form-item label="用户名">
+              <el-input v-model="formdata.username"></el-input>
+            </el-form-item>
+            <el-form-item label="密码">
+              <el-input v-model="formdata.password"></el-input>
+            </el-form-item>
+            <el-form-item label="邮箱">
+              <el-input v-model="formdata.email"></el-input>
+            </el-form-item>
+            <el-form-item label="电话">
+              <el-input v-model="formdata.mobile"></el-input>
+            </el-form-item>
+          </el-form>
+          <div slot="footer" class="dialog-footer">
+            <el-button @click="dialogFormVisibleAdd = false">取 消</el-button>
+            <el-button type="primary" @click="dialogFormVisibleAdd = false">确 定</el-button>
+          </div>
+        </el-dialog>
 
     </el-card>
 </template>
@@ -66,15 +97,48 @@ export default {
         return {
             query:'',
             pagenum:1,
-            pagesize:10,
+            pagesize:2,
             list:[],
-
+            total:-1,
+            dialogFormVisibleAdd:false,
+            // 添加表单数据
+            formdata:{
+                username:'',
+                password:'',
+                email:'',
+                mobile:''
+            }
         }
     },
     created() {
         this.getTableData()
     },
     methods: {
+        // 添加用户对话框
+        showDiaAddUsers(){
+            this.dialogFormVisibleAdd=true
+        },
+        // 搜索
+        getAllUsers(){
+            this.getTableData()
+        },
+        searchUser(){
+            this.getTableData()
+        },
+        // 分页方法
+        handleSizeChange(val) {
+          console.log(`每页 ${val} 条`);
+        //   每页几条请求
+        this.pagenum = 1
+        this.pagesize = val
+        this.getTableData()
+        },
+        handleCurrentChange(val) {
+          console.log(`当前页: ${val}`);
+        //   根据新页码发送请求
+        this.pagenum = val
+        this.getTableData()
+        },
         // 设置请求头
         async getTableData(){
             const AUTH_TOKEN = localStorage.getItem('token')
@@ -83,6 +147,7 @@ export default {
             console.log(res)
             const {data,meta:{status,msg}} = res.data
             if(status===200){
+                this.total = data.total
                 this.list = data.users
             }
 
